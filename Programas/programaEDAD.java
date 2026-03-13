a21ed61ccae9b68bf91ca681405c05a6c8c2dd8b
Juan Andres <juanandres190507@gmail.com>
2026-03-13 09:27:21 -0500

    BONIFICACION HORA DE CLASE EJERCICIO 2

---
+++ Programas/programaEDAD.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class programaEDAD {
+
+public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    int edad = 0;
+
+    System.out.println("Ingrese su edad: ");
+    edad = sc.nextInt();
+
+    if (edad >= 18) {
+
+        System.out.println("<(^_^)>   Puedes entrar a la fiesta");
+        
+    } else {
+        
+        System.out.println("¯\\_(:/)_/¯    No puedes ingresar, eres menor de edad");
+    }
+
+
+
+
+
+
+}
+
+
+
+
+}
