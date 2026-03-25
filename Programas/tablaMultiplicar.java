8adec85a3e6c3e753b1de10c0cb48c9fca49e045
Juan Andres <juanandres190507@gmail.com>
2026-03-24 20:59:57 -0500

    PROGRAMA TABLAS TERMINADO

--- Programas/tablaMultiplicar.java
+++ Programas/tablaMultiplicar.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
 
 public class tablaMultiplicar {
-    public static void main(String[] args) {
+ public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Pedir número al usuario
+        System.out.print("Ingrese un número: ");
+        int numero = sc.nextInt();
+
+        // Mostrar tabla de multiplicar
+        System.out.println("Tabla de multiplicar del " + numero + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+
+        sc.close();
     }
+
 }
